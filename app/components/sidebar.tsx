d1bc9131f382c16ef368996117e681916d35a79a
muxinxy <zeng5360@gmail.com>
2023-11-10 17:34:30 +0800

    Chinese

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          在线智能聊天
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          你的生活、学习和工作智能助手。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
